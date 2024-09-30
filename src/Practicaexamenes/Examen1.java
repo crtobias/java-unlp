27c57986589cf7dc709e76852313bc3c3de51728
crtobias <tgonzalezarriola@gmail.com>
2024-09-30 20:25:47 -0300

    asd

--- src/Practicaexamenes/Examen1.java
+++ src/Practicaexamenes/Examen1.java
@@ -4,25 +4,83 @@ package Practicaexamenes;
 // https://drive.google.com/drive/folders/1GZniXTM3lK3YbRfANWw4uEDnE3IztOkJ 
 
 
-//clase empresa
+
+
+//clase empresa(n,nombre,direccion)
 //      
 //        nombre
 //        direccion
 //
-//        director
+
+// persona donde deriva dirctor y encargado 
+
+
+//  clase director
 //        nombre 
 //        dni 
 //        ingreso 
-//        sueldo basico
-//        monto viaticos
+//        sueldo
+//  unic  monto viaticos
 //
-//        encargados de sucursales 1..n
-//        nnombre dni
+//    (n)  array de encargador
+//  clase encargados de sucursales 1..n
+//        nombre
+//        dni
 //        ingreso 
 //        sueldo
-//        empleados
+//  unic  empleados
 //        
         
-public class Examen1 {
+public class Empresa {
+    private String nombre;
+    private String direccion;
+    private int sucursales;
     
+    //array de encargadors hasta sucursales
+    //variable de director
+
+    public Empresa(String nom,String dire,int s) {
+        this.nombre=nom;
+        this.direccion=dire;
+        this.sucursales=s;
+    }
+}
+
+public class Persona{
+    private String nombre;
+    private int dni;
+    private int sueldo;
+    private int ingreso;
+    
+    
+    public Persona(String n, int d, int s, int i){
+        this.nombre = n;
+        this.dni =d;
+        this.sueldo = s;
+        this.ingreso = i;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+}
+
+
+public class Encargado extends Persona{
+    private int empleados;
+    
+       public Encargado(String n, int d, int s, int i,int emp){
+           super(n,d,s,i);
+           this.empleados=emp;
+       }
+}
+
+
+public class Director extends Persona{
+    private int viaticos;
+    
+       public Director(String n, int d, int s, int i,int via){
+           super(n,d,s,i);
+           this.viaticos=via;
+       }
 }
---
+++ src/Practicaexamenes/NewClass.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Practicaexamenes;
+import PaqueteLectura.Lector; 
+
+public class NewClass {
+    private Agenda agenda= new Agenda();
+    private String nombre;
+    private boolean social;
+    private double costo;
+    
+    
+    
+    
+    
+    for(int i =  ;i<5 ;i++){
+    
+    nombre = Lector.leerString();
+    social = Lector.leerBoolean();
+    costo = Lector.leerDouble();
+    
+    private Paciente pac = new Paciente();
+    
+    agenda.agendarTurno();
+    agenda.liberarTurno();
+    agenda.tiene();
+    
+    
+    
+}  
+}
