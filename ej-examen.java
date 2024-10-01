b998fec1f26d98f45dc38003c803596cb31367b7
tobias gonzalez arriola <100397465+crtobias@users.noreply.github.com>
2024-10-01 08:32:55 -0300

    Create ej-examen.java

---
+++ ej-examen.java
@@ -0,0 +1,21 @@
+//link:
+//https://drive.google.com/drive/folders/1GZniXTM3lK3YbRfANWw4uEDnE3IztOkJ
+
+package practica;
+
+//nombre torneo
+
+//clase de fechas para herencia
+
+//tabla de 1..n fechas
+//		m goleadores 0..m-1 goleadores
+
+//tabla de 1..n fechas 
+//	goleadores
+//		nombre
+//		equipo
+//		goles en fecha
+
+public class Goleadores {
+	
+}
